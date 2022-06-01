090363f620d09726244b6703c9823cd9be873cf8
Vladislav Moruga <Monctpik123@gmail.com>
2022-06-01 14:40:40 +0300

    example prompt

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-312 
-"qwe"
-true
-alert(20)
-alert("i'm a teacher")
-alert('i\'m a teacher');
+const userInputString = prompt('Enter number', 12);
+const userInputNumber = Number(userInputString);
+
+if(userInputString===null) {
+    console.log('error')
+}else{
+    console.log('thanks for', userInputString)
+}
